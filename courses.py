3807857e99d3f3d5177a3b75ed17b050c8119cd6
Tyler Westland <tylerofthewest@gmail.com>
2020-02-19 10:21:08 -0500

    Adds courses class.

---
+++ courses.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+__author__="Tyler Westland"
+
+import argparse
+import os
+
+# Course class
+class Course:
+    def __init__(self, department, number, name, credits,  grade, term, year):
+        self.credits = credits
+        self.department = department
+        self.grade = grade
+        self.name = name
+        self.number = number
+        self.term = term
+        self.year = year
+
+    def __str__(self):
+        return "{self.department}{self.number} -- {self.name} -- "\
+                "{self.credits} -- {self.grade} -- {self.term}-{self.year}"\
+                .format(self=self)
+
+
+# Courses taken
+COMPLETED = [
+    # Fall 2018
+    Course("CS", 6052, "INTLDATAANALYSIS", 3.0, "B+", "fall", 2018),
+    Course("CS", 6070, "AUTOMATA", 3.0, "B+", "fall", 2018),
+    Course("CS", 6097, "WIRELESS NET", 3.0, "B+", "fall", 2018),
+    Course("CS", 7001, "CS SEMINAR I", 1.0, "P", "fall", 2018),
+    Course("CS", 7080, "SELF STUDY RESEARCH", 5.0, "P", "fall", 2018),
+    # Spring 2019
+    Course("CS", 6038, "MALWARE ANALYSIS", 3.0, "A", "spring", 2019),
+    Course("CS", 7002, "CS SEMINAR II", 1.0, "P", "spring", 2019),
+    Course("CS", 7080, "SELF STUDY RESEARCH", 4.0, "P", "spring", 2019),
+    Course("EECE", 7004, "PRACTICAL EXPER", 1.0, "P", "spring", 2019),
+    Course("EECE", 7019, "BIO-INSPIRED ROBOTICS", 3.0, "A", "spring", 2019),
+    Course("EECE", 7065, "COMPLEX SYS/NETWORKS", 3.0, "A", "spring", 2019),
+    # Fall 2019
+    Course("CS", 6054, "INFO RETRIEVAL", 3.0, "B", "fall", 2019),
+    Course("CS", 7081, "ADV. ALGORITHMS I", 3.0, "A-", "fall", 2019),
+    Course("EECE", 6036, "INTELLIGENT SYSTEMS", 3.0, "C", "fall", 2019),
+    Course("EECE", 9089, "THESIS/DIS RESEARCH", 6.0, "P", "fall", 2019)
+]
+
+
+
+def main(args=None):
+    """Main function of this file
+
+    Arguments:
+    args: List of strings to be parsed by argparse.
+        None results in argparse using the values passed into
+        sys.args.
+    """
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args(args=args)
+
+    for course in COMPLETED:
+        print(course)
+
+    # Return success code
+    return 0
+
+
+# Execute only if this file is being run as the entry file.
+if __name__ == "__main__":
+    import sys
+    exit(main())
+
